aa6f4ec2b5204703618c5649da876eef2f804b09
Cao Minh Duc <caominhduc31081@gmail.com>
2020-08-01 19:31:13 +0700

    update config default

--- config/config.php
+++ config/config.php
@@ -6,6 +6,10 @@
 return [
     'logo' => '',
     'features' => [
-        ''
+        [
+            'name' => 'Home',
+            'route' => 'home',
+            'route_prefix' => 'home'
+        ]
     ] 
 ];
