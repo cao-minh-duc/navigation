1022c052a3c4e2cab9a10a46e06b340a49331e05
Cao Minh Duc <caominhduc31081@gmail.com>
2020-08-01 20:09:42 +0700

    add dropdown

--- resources/views/basic.blade.php
+++ resources/views/basic.blade.php
@@ -1,6 +1,5 @@
-
-<nav class="bg-default-800">
-    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+<nav class="bg-default-800" x-data="{ open: false }">
+    <div class="px-4 mx-auto max-w-7xl sm:px-6">
         <div class="flex items-center justify-between h-16">
             <div class="flex items-center">
                 <div class="flex-shrink-0">
@@ -27,7 +26,8 @@
             <div class="flex mr-2 md:hidden">
             <!-- Mobile menu button -->
                 @include('navigation::logout')
-                <button class="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-default-700 focus:outline-none focus:bg-default-700 focus:text-white">
+                <button @click="open = true"
+                    class="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-default-700 focus:outline-none focus:bg-default-700 focus:text-white">
                     <!-- Menu open: "hidden", Menu closed: "block" -->
                     <svg class="block w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -46,7 +46,7 @@
 
     Open: "block", closed: "hidden"
     -->
-    <div class="block md:hidden">
+    <div class="md:hidden" x-show="open" @click.away="open = false">
         <div class="px-2 pt-2 pb-3 sm:px-3">
             
             @foreach($features as $feature)
